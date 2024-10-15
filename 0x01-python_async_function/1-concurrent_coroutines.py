3eae6a6ff477f6d38ca2d1d78a3c26aef2e60aaa
victorpropython <victorchibuzor3109@gmail.com>
2024-10-15 12:55:29 +0100

    for task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,22 +1,32 @@
 #!/usr/bin/env python3
 import asyncio
 import random
+from heapq import heappush, heappop
+from typing import List
 
-wait_random = __import__ ('0-basic_async_syntax')
+# importing wait_random
+# from basic_async_syntax import wait_random
 
-async def wait_n(n, max_delay):
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+
     """
     Asynchronous function to spawn n no of times,
     with specified max_delay.
 
-    Args, n and max_delay. Where n is the integer 
-    number of times and max_delay is the maximum 
+    Args, n and max_delay. Where n is the integer
+    number of times and max_delay is the maximum
     number of delay.
     """
-    stalling = random.uniform(0, max_delay)
+    # Creating a list
+    delay = []
 
-    await asyncio.sleep(stalling)
+    # Creating a list for awaitable tasks
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    # Run all taks concurrently and get result
+    delay = await asyncio.gather(*tasks)
 
-    #print(wait_n(range(n,max_delay)))
-
-    return stalling#wait_n(n, max_delay)
+    # Returning the delays
+    return sorted(delay)
