ee158b206f5567d355a9e2dc6168030619192530
victorpropython <victorchibuzor3109@gmail.com>
2024-10-15 12:17:05 +0100

    for task 0

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -2,9 +2,11 @@
 import asyncio
 import random
 
-async def wait_random(max_delay: int = 10)-> float:
+
+async def wait_random(max_delay: int = 10) -> float:
+
     """
-    Asynchronous coroutine that waits for a random delay 
+    Asynchronous coroutine that waits for a random delay
     between 0 and max_delay seconds and return the actual delay
     time.
 
@@ -24,4 +26,3 @@ async def wait_random(max_delay: int = 10)-> float:
 
     # run the code
     return delay
-     
--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,1 +1,22 @@
+#!/usr/bin/env python3
+import asyncio
+import random
+
 wait_random = __import__ ('0-basic_async_syntax')
+
+async def wait_n(n, max_delay):
+    """
+    Asynchronous function to spawn n no of times,
+    with specified max_delay.
+
+    Args, n and max_delay. Where n is the integer 
+    number of times and max_delay is the maximum 
+    number of delay.
+    """
+    stalling = random.uniform(0, max_delay)
+
+    await asyncio.sleep(stalling)
+
+    #print(wait_n(range(n,max_delay)))
+
+    return stalling#wait_n(n, max_delay)
