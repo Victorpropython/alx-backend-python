e042f9c773fb484f4e13092e1ae936a888750e56
victorpropython <victorchibuzor3109@gmail.com>
2024-10-16 07:17:23 +0100

    for task 0 corrections

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -8,6 +8,7 @@ from importlib import import_module as using
 
 async_comprehension = using('1-async_comprehension').async_comprehension
 
+
 async def measure_runtime() -> float:
     """
     An async function to check for time used in
