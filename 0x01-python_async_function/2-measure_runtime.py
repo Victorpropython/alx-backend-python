2da3720f8e7f0452f2c18448b5b2225fa4a33e84
victorpropython <victorchibuzor3109@gmail.com>
2024-10-18 23:38:13 +0100

    fortask 3

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Function to measure the total time of execution for wait_n(n, max_delay)
+And return total_time
+"""
+import asyncio
+import time
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+async def measure_time(n: int, max_delay: int) -> [float]:
+    """
+    Funtion to calculate the total time elasped
+    """
+    start_time = time.time() # The start time
+    asyncio.run(wait_n(n, max_delay)) # calling wait_n function
+    return (time.time() - start-time) / n
